0bb58008309d9a55a6559d4f91724a343e63c6ae
kimnguyenthihoang916@gmail.com <23110248@student.hcmute.edu.vn>
2026-05-11 00:26:16 +0700

    upload bổ sung chức năng login

--- src/models/User.js
+++ src/models/User.js
@@ -86,7 +86,4 @@ const userSchema = new mongoose.Schema({
   timestamps: true 
 });
 
-// Index để tối ưu query
-userSchema.index({ email: 1 });
-
 module.exports = mongoose.model('User', userSchema);
