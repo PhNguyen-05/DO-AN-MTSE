289720eb0808bd50e7f65165bdfa177c04c0c9d8
Trần Hồ Phương Nguyên <phng972005@gmail.com>
2026-05-10 23:39:43 +0700

    fix: conflict login, register

--- src/server.js
+++ src/server.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const ejsLayouts = require('express-ejs-layouts');
 
 require('dotenv').config();
 
 const connectDB = require('./config/db');
-const authRoutes = require('./routes/auth.routes');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(ejsLayouts);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
+app.set('layout', 'layouts/main');
 
 connectDB();
-
 
 app.use('/', authRoutes);
 
